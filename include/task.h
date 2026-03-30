aaa43f947204ffdcefcf1f1593e27942142e2e81
kstribrnAmzn <kstribrnAmzn@users.noreply.github.com>
2026-03-30 23:30:33 +0000

    [AUTO][RELEASE]: Bump task.h version macros to "12.1.2"

--- include/task.h
+++ include/task.h
@@ -54,10 +54,10 @@
  * The tskKERNEL_VERSION_MAJOR, tskKERNEL_VERSION_MINOR, tskKERNEL_VERSION_BUILD
  * values will reflect the last released version number.
  */
-#define tskKERNEL_VERSION_NUMBER                      "V11.1.0+"
-#define tskKERNEL_VERSION_MAJOR                       11
+#define tskKERNEL_VERSION_NUMBER                      "V12.1.2"
+#define tskKERNEL_VERSION_MAJOR                       12
 #define tskKERNEL_VERSION_MINOR                       1
-#define tskKERNEL_VERSION_BUILD                       0
+#define tskKERNEL_VERSION_BUILD                       2
 
 /* MPU region parameters passed in ulParameters
  * of MemoryRegion_t struct. */
